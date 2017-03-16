4e2ec5e6916469587b7bf9cbab5f5e28f4ceebe9
swenmeeuwes <swenmeeu@gmail.com>
2017-03-16 15:57:48 +0100

    Seperated template from component

--- CHIBB_API_DASHBOARD/src/app/app.component.ts
+++ CHIBB_API_DASHBOARD/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app/app.component.html',
 })
 export class AppComponent  { name = 'Angular'; }
