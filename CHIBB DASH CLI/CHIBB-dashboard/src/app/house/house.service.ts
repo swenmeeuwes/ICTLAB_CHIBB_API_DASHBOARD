a483c59e1337716bc9f0e4de732faddd7af0f792
Swen Meeuwes <swenmeeu@gmail.com>
2017-06-08 14:58:35 +0200

    Remove console.log from house services

--- CHIBB DASH CLI/CHIBB-dashboard/src/app/house/house.service.ts
+++ CHIBB DASH CLI/CHIBB-dashboard/src/app/house/house.service.ts
@@ -51,7 +51,6 @@ export class HouseService {
 
             resultObservable.subscribe(
                 data => {
-                    console.log(data);
                     resolve(data["responseCode"]);
                 },
                 error => { reject(error.json()["responseCode"]) },
